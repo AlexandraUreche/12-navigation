4a20ad0ebfe7e41a9d5c38f33575d33b97a8afc4
AlexandraUreche <70063621+AlexandraUreche@users.noreply.github.com>
2020-10-14 21:29:59 +0300

    reward,account and payment pages are done

--- resources/main.js
+++ resources/main.js
@@ -2,13 +2,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     const menuItems = document.getElementsByClassName("menu-item");
     const imgItems=document.getElementsByClassName("img-item");
-    for (let i = 0; i < menuItems.length-1; i++) {
+    for (let i = 0; i < menuItems.length; i++) {
         const menuItem = menuItems[i];
         const imgItem=imgItems[i];
         menuItem.addEventListener('click', function() {
-            for(let j = 0; j < menuItems.length-1; j++) {
+            for(let j = 0; j < menuItems.length; j++) {
                 if(menuItems[j].classList.contains('selected')){
                     menuItems[j].classList.remove("selected");
+                    menuItems[j].style.cssText="border:none;";
                 } else {
                     continue;
                 }
@@ -17,8 +18,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             imgItem.classList.add("selected");
             const dataContent = menuItem.dataset.content;
             const contentElements = document.getElementsByClassName("content");
-            console.log(contentElements);
-            for(let k = 0; k < contentElements.length-1; k++) {
+            for(let k = 0; k < contentElements.length; k++) {
                 if(contentElements[k].classList.contains('hidden')) {
                     continue;
                 } else {
@@ -26,6 +26,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 }
             }
             document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
+            if(menuItem.classList.contains("selected")){
+                menuItem.style.cssText="border-bottom: 7px solid rgb(233,224,217);";
+            }
             if(imgItem.classList.contains("selected")){
                 imgItem.classList.remove("hidden");
             }
